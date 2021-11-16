f114f656a98e140acb0164e4f07c37c2b4f24381
no one <adambimek@gmail.com>
2021-11-16 23:10:04 +0100

    helper class

---
+++ src/mup/nolan/mupplugin/utils/Resrc.java
@@ -0,0 +1,34 @@
+package mup.nolan.mupplugin.utils;
+
+public class Resrc<T>
+{
+	private T resource;
+
+	public Resrc()
+	{
+
+	}
+
+	public Resrc(T resource)
+	{
+		this.resource = resource;
+	}
+
+	public T get()
+	{
+		return resource;
+	}
+
+	public void set(T resource)
+	{
+		this.resource = resource;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Resrc{" +
+				"resource=" + resource +
+				'}';
+	}
+}
