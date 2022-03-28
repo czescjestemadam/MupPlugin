719763c787fb5b2ff45b23d1669d03843caa928d
nonek <adambimek@gmail.com>
2022-03-28 18:35:54 +0200

    fixing all anvils

--- src/mup/nolan/mupplugin/modules/UnbreakableanvilsModule.java
+++ src/mup/nolan/mupplugin/modules/UnbreakableanvilsModule.java
@@ -1,5 +1,15 @@
 package mup.nolan.mupplugin.modules;
 
+import mup.nolan.mupplugin.MupPlugin;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Directional;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+
 public class UnbreakableanvilsModule extends Module
 {
 	public UnbreakableanvilsModule(MupPlugin mupPlugin)
@@ -17,6 +27,22 @@ public class UnbreakableanvilsModule extends Module
 		if (inv.getType() != InventoryType.ANVIL)
 			return;
 
-		
+		final Location loc = inv.getLocation();
+		if (loc != null)
+			fix(loc.getBlock());
+
+	}
+
+	private void fix(Block anvil)
+	{
+		Directional data = (Directional)anvil.getBlockData();
+		final BlockFace facing = data.getFacing();
+
+		if (anvil.getType() != Material.ANVIL)
+			anvil.setType(Material.ANVIL);
+
+		data = (Directional)anvil.getBlockData();
+		data.setFacing(facing);
+		anvil.setBlockData(data);
 	}
 }
