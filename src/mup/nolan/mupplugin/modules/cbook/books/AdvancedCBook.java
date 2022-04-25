81cff67958b505006e8619ee9a91af231044b3a2
nonek <adambimek@gmail.com>
2022-04-25 22:14:03 +0200

    AdvancedCBook tag interpreter

--- src/mup/nolan/mupplugin/modules/cbook/books/AdvancedCBook.java
+++ src/mup/nolan/mupplugin/modules/cbook/books/AdvancedCBook.java
@@ -2,10 +2,14 @@ package mup.nolan.mupplugin.modules.cbook.books;
 
 import com.google.common.collect.Lists;
 import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.inventory.meta.BookMeta;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AdvancedCBook extends BaseCBook
 {
@@ -58,36 +62,109 @@ public class AdvancedCBook extends BaseCBook
 
 	private static class Tag
 	{
-		final String name;
+		final String raw;
+		boolean returnRaw = false;
+		String name;
 		final Map<String, Object> props = new HashMap<>();
+		final List<String> flags = new ArrayList<>();
 
-		public Tag(String name)
+		public Tag(String tag)
 		{
-			this.name = name;
+			raw = tag;
+
+			tag = tag.substring(1, tag.length() - 1).trim();
+			final int nameLen = tag.indexOf(' ');
+			if (nameLen < 0 || tag.isEmpty())
+			{
+				name = raw;
+				returnRaw = true;
+				return;
+			}
+
+			name = tag.substring(0, nameLen);
+			tag = tag.substring(nameLen + 1);
+
+			boolean qs = false;
+			boolean val = false;
+			String buff = "";
+			String prop = "";
+			for (char c : tag.toCharArray())
+			{
+				if (c == '\'' || c == '"')
+					qs = !qs;
+
+				if (qs)
+					buff += c;
+				else if (c == '=')
+				{
+					prop = buff;
+					buff = "";
+					val = true;
+				}
+				else if (c == ' ')
+				{
+					if (val)
+					{
+						putProperty(buff, prop);
+						prop = "";
+						buff = "";
+						val = false;
+					}
+					else
+					{
+						flags.add(buff);
+						buff = "";
+					}
+				}
+				else
+					buff += c;
+			}
+
+			if (val)
+				putProperty(buff, prop);
 		}
 
-		public void addProperty(String prop, Object val)
+		private void putProperty(String buff, String prop)
 		{
-			props.put(prop, val);
+			Object value;
+			if ((buff.charAt(0) == '\'' || buff.charAt(0) == '"') && (buff.charAt(buff.length() - 1) == '\'' || buff.charAt(buff.length() - 1) == '"'))
+				value = buff.substring(1, buff.length() - 1);
+			else
+				value = Integer.parseInt(buff);
+			props.put(prop, value);
 		}
 
 		public TextComponent toComponent()
 		{
+			final TextComponent tx = new TextComponent();
 
-		}
+			tx.setText((String)props.get("text"));
 
-		public static Tag fromString(String tag)
-		{
-			String prop;
-			Object val;
-
-			String buff = "";
-			for (char c : tag.toCharArray())
+			switch (name)
 			{
-
+				case "link" -> link(tx);
 			}
 
-			return new Tag("");
+			return tx;
+		}
+
+		private void link(TextComponent tx)
+		{
+			tx.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, (String)props.get("url")));
+			if (flags.contains("u"))
+				tx.setUnderlined(true);
+		}
+
+		@Override
+		public String toString()
+		{
+			return "Tag{" +
+					"raw='" + raw + '\'' +
+					", returnRaw=" + returnRaw +
+					", name='" + name + '\'' +
+					", props=" + props +
+					", flags=" + flags +
+					'}';
 		}
 	}
 }
